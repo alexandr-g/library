8863b49fd50aef4ae0fb07f26e7a22fec85b7ca9
Alexandr <alexgrischuk@gmail.com>
2016-02-22 16:11:55 +0100

    Add admin path and mongodb connection

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var adminRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+
+var router = function (nav) {
+  var books = [
+      {
+          title: 'War and Peace',
+          genre: 'Historical Fiction',
+          author: 'Lev Nikolayevich Tolstoy',
+          read: false
+      },
+      {
+          title: 'Les Miserables',
+          genre: 'Historical Fiction',
+          author: 'Victor Hugo',
+          read: false
+      },
+      {
+          title: 'Harry Potter and the Cursed Child',
+          genre: 'Dramatik',
+          author: 'Joanne K. Rowling',
+          read: false
+      }
+  ];
+
+  adminRouter.route('/addBooks')
+    .get(function(req, res) {
+        var url = 'mongodb://localhost:27017/libraryApp';
+
+        mongodb.connect(url, function(err, db) {
+            var collection = db.collection('books');
+                collection.insertMany(books,
+                    function(err, results) {
+                      res.send(results);
+                      db.close();
+                    });
+        });
+
+        // res.send('inserting books');s
+    });
+
+  return adminRouter;
+};
+
+module.exports = router;
